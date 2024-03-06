5556f0b75e81b4a98940e69d9f10e05a07b3c894
Emanuel Morales <101947718+Kuro-98@users.noreply.github.com>
2024-03-06 12:23:34 -0600

    Ajustar el desplazamiento de las imágenes en el evento de desplazamiento

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 const imagenes = document.querySelectorAll('.propiedad__imagen');
 
-window.addEventListener('scroll', function () {
-	const scroll = this.scrollY / -20;
-
-	imagenes.forEach((imagen) => {
-		imagen.style.backgroundPositionY = `${scroll}px`;
+window.addEventListener('scroll', () => {
+	let scroll = this.scrollY / -20;
+	imagenes.forEach((img) => {
+		if (scroll < -80) {
+			scroll += 80;
+		}
+		img.style.backgroundPositionY = `${scroll}px`;
 	});
 });
